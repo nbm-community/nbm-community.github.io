79c4d6c238108209ccd4372977e0ed0c6b1e3a67
dev <user@users.noreply.github.com>
2021-06-03 16:38:22 +0300

    58

--- zh/assets/js/lunr/lunr-store.js
+++ zh/assets/js/lunr/lunr-store.js
@@ -5,10 +5,10 @@ var store = [{
         "url": "https://nbminer.info/documentation/setup/",
         "teaser": "https://nbminer.info/assets/images/thumbs/500x300.png"
       },{
-        "title": "Download TeamRedMiner 0.8.3 - AMD GPU Miner [2020]",
-        "excerpt":" Get started with TeamRedMiner  \t\t \t\t\t \t\tVersion: 37.5  \t\tFile: *TeamRedMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t    The new TeamRedMiner beta version with ETCHash support is ready.    You can download TeamRedMiner 0.8.3 from here:   Download Now   or:   Mega (mirror)   Inside the archive you will find a file README.txt with installation instructions.   ","categories": ["download"],
-        "tags": ["TeamRedMiner"],
-        "url": "https://nbminer.info/download/0.8.3/",
+        "title": "Download NBMiner  37.5 - AMD GPU Miner [2021]",
+        "excerpt":" Get started with NBMiner  \t\t \t\t\t \t\tVersion: 37.5  \t\tFile: *NBMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new NBMiner stale version is ready.   You can download NBMiner 37.5 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "tags": ["NBMiner"],
+        "url": "https://nbminer.info/download/37.5/",
         "teaser": "https://nbminer.info/assets/images/thumbs/500x300.png"
       },{
         "title": "NBMiner&#58; 命令行参数",
