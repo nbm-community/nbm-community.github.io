43f39c5de996c11880fed60e01f59dc70bb11c3b
dev <user@users.noreply.github.com>
2021-11-05 20:38:33 +0300

    zilla

--- es/assets/js/lunr/lunr-store.js
+++ es/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "https://nbminer.info/assets/images/thumbs/500x300.png"
       },{
         "title": "Download NBMiner  37.5 - AMD GPU Miner [2021]",
-        "excerpt":" Get started with NBMiner  \t\t \t\t\t \t\tVersion: 37.5  \t\tFile: *NBMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new NBMiner stale version is ready.   You can download NBMiner 37.5 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "excerpt":" Get started with NBMiner  \t\t \t\t\t \t\tVersion: 37.5  \t\tFile: *NBMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new NBMiner stale version is ready.         You can download NBMiner 37.5 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
         "tags": ["NBMiner"],
         "url": "https://nbminer.info/download/37.5/",
         "teaser": "https://nbminer.info/assets/images/thumbs/500x300.png"
