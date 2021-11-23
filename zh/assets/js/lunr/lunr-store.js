6ca4344502bf1c9bc716351eb3d6570233b5df4a
dev <user@users.noreply.github.com>
2021-11-23 16:47:26 +0300

    40

--- zh/assets/js/lunr/lunr-store.js
+++ zh/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "NBMiner Setup Guide [2021]",
-        "excerpt":"You can use this list as a reference for all Launch Parameters supported by NBMiner.          You can download NBMiner 39.7 from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace NBMiner.exe with ./NBMiner in the command-line examples below.   ","categories": ["documentation"],
+        "excerpt":"You can use this list as a reference for all Launch Parameters supported by NBMiner.          You can download NBMiner 40.0 from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace NBMiner.exe with ./NBMiner in the command-line examples below.   ","categories": ["documentation"],
         "tags": ["NBMiner"],
         "url": "https://nbminer.info/documentation/setup/",
         "teaser": "https://nbminer.info/assets/images/thumbs/500x300.png"
